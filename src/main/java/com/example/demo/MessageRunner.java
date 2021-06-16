91f17b0c09d2f46b633aec1a9d84922896ea60f8
0015OO744 <0015OO744@LAPTOP-KHM06BPO>
2021-06-16 18:09:11 +0530

    second commit

--- src/main/java/com/example/demo/MessageRunner.java
+++ src/main/java/com/example/demo/MessageRunner.java
@@ -9,6 +9,8 @@ public class MessageRunner implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome To Springboot App ");
+		System.out.println("Hello Modified ");
+
 
 	}
 
